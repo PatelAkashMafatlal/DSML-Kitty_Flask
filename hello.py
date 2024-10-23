275cc8352981ce969d33cb9bcec0c684b0ba837c
PatelAkashMafatlal <156079266+PatelAkashMafatlal@users.noreply.github.com>
2024-10-23 18:40:45 +0530

    Post req added

---
+++ hello.py
@@ -0,0 +1,12 @@
+# noinspection PyUnresolvedReferences
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/hello")
+def hello_world():
+    return "<p>I have triggered hello world function</p>"
+
+@app.route("/")
+def main_page():
+    return "<H1>WELCOME TO MY WEBSITE</H1>"
